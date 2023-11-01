69a61640b475893cecb072475718b873f5647260
addobosz <adamdobosz.poczta@gmail.com>
2023-11-01 11:40:12 +0100

    added uml_diagram

--- race.cpp
+++ race.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+
+class Vehicle {
+public:
+    Vehicle(int distance, int numberOfSeats, int avgSpeed, const std::string& name, int price):
+        mDistance(distance), mNumberOfSeats(numberOfSeats), mAvgSpeed(avgSpeed), mName(name), mPrice(price)
+    {}
+};
+
+int main() {
+    char name[] = "elton";
+    Vehicle brr = Vehicle(1,1,1,name,1);
+    std::cout << brr.distance;
+    
+}
