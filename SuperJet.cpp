19de9e4be7f535726d8b087a4f6002f1e8368fb8
addobosz <adamdobosz.poczta@gmail.com>
2023-11-14 13:58:20 +0100

    Update SuperJet.cpp

--- SuperJet.cpp
+++ SuperJet.cpp
@@ -12,6 +12,20 @@ void SuperJet::setTurbo(bool turbo) {
     mTurbo = turbo;
 }
 
+void SuperJet::startEngine() {
+    std::cout << mName << "'s engine has just started!" << std::endl;
+}
+
+void SuperJet::stopEngine() {
+    std::cout << mName << "'s engine has just stopped!" << std::endl;
+}
+
+void SuperJet::dive() {
+    std::cout << mName << " is attempting a dive!" << std::endl;
+    mDistance += 100;
+}
+
+
 std::ostream& operator<<(std::ostream& os, const SuperJet& superjet) {
     os << static_cast<const Air&>(superjet);
     os << "Turbo: " << superjet.getTurbo() << std::endl;
